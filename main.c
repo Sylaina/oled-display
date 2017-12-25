ca79fdbabb927a514cd79691f45387c0bcf3f902
Sylaina <skie_@web.de>
2017-12-25 15:09:08 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,22 +7,23 @@
 
 #include <stdlib.h>
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #include "i2c.h"
 #include "lcd.h"
 
 int main(void)
 {
-    /* insert your hardware initialization here */
+    /* init I2C-Bus */
     i2c_init();
-    
+    /* init display, after init turn on */
     lcd_init(LCD_DISP_ON);
+    /* clear screen */
     lcd_clrscr();
+    /* put string from flash, cursor is at position x=0, y=0, left, top corner */
     lcd_puts_p(PSTR("OLED Display "));
+    /* set cursor at begin from next line */
     lcd_gotoxy(0,1);
-    lcd_puts_p(PSTR("with I2C "));
-    
-    sei();
+    /* put string from sram */
+    lcd_puts_p("with I2C ");
     
     for(;;){
         /* insert your main loop code here */
