06ac702c66bd1eb91c0600a6d1555c933e32e68d
Sylaina <skie_@web.de>
2018-09-17 11:21:31 +0200

    Update font.h

--- font.h
+++ font.h
@@ -1,6 +1,6 @@
 /*
  *  font.h
- *  
+ *  i2c
  *
  *  Created by Michael Köhler on 13.09.18.
  *  Copyright 2018 Skie-Systems. All rights reserved.
@@ -8,10 +8,9 @@
  */
 #ifndef _font_h_
 #define _font_h_
-
 #include <avr/pgmspace.h>
 
 extern const char ssd1306oled_font[][6] PROGMEM;
-uint8_t getCharPosition(char value);
+extern const char special_char[][2] PROGMEM;
 
 #endif
